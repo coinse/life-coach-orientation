7f5aa5f9f063e1c7c5f069d5418e1412cccaf0b8
Juyeon <juyeon.yoon@kaist.ac.kr>
2025-11-28 15:40:34 +0900

    Update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 // const banner = <Banner storageKey="some-key">This template was created with 🩸 and 💦 by <Link href="https://github.com/phucbm">PHUCBM</Link> 🐧</Banner>
 const navbar = (
     <Navbar
-        logo={<img src="/images/general/sia_logo.png" alt="Logo" width={100} height={20}/>}
+        logo={<img src="/images/general/sia_logo_orientation.svg" alt="Logo" width={200} height={30}/>}
         // ... Your additional navbar options
     />
 )
